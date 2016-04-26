861d36072a605d1b74ccffa7dfdbce08de9044a3
jbandura <jacek.bandura@netguru.pl>
2016-04-26 12:24:45 +0200

    fix jumping buttons problems [#117989541]

--- scripts/app.js
+++ scripts/app.js
@@ -26,37 +26,41 @@ $(function() {
     return false;
   });
 
-  var $startElement = $('.js-sticky-container'),
-      $stopElement = $('.js-scroll-stop'),
-      coverOffset = $('.js-post-cover').offset().top,
-      stickyClass = 'post__sticky--stick',
-      offset = 50,
-      textPadding = 16,
-      startSticky = $startElement.offset().top - offset,
-      stopSticky = $stopElement.offset().top - offset - textPadding - $startElement.height(),
-      postPosition = $('.post__content'),
-      scrollTop, stopOffset;
+  if ($('.post').length > 0) {
+    var $startElement = $('.js-sticky-container'),
+        $stopElement = $('.js-scroll-stop'),
+        coverOffset = $('.js-post-cover').offset().top,
+        stickyClass = 'post__sticky--stick',
+        offset = 50,
+        textPadding = 16,
+        startSticky = $startElement.offset().top - offset,
+        postPosition = $('.post__content'),
+        scrollTop, stopOffset, stopSticky;
 
-  $(window).scroll(function(){
-    scrollTop = $(window).scrollTop();
-    if (scrollTop > startSticky && scrollTop < stopSticky) {
-      $startElement.addClass(stickyClass)
-        .attr('style', null)
-        .css({
-          left: (postPosition.offset().left - 100) + 'px'
+    $('.post').imagesLoaded(function() {
+      stopSticky = $('.js-scroll-stop').offset().top - offset - textPadding - $startElement.height();
+    });
+
+    $(window).scroll(function(){
+      scrollTop = $(window).scrollTop();
+      if (scrollTop > startSticky && scrollTop < stopSticky) {
+        $startElement.addClass(stickyClass)
+          .attr('style', null)
+          .css({
+            left: (postPosition.offset().left - 100) + 'px'
+          });
+      }
+      else if(scrollTop > stopSticky) {
+        stopOffset = $stopElement.offset().top - coverOffset - $startElement.height() - textPadding;
+        $startElement.removeClass(stickyClass);
+        $startElement.css({
+          top: stopOffset + 'px',
+          position: 'absolute',
+          left: '-100px'
         });
-    }
-    else if(scrollTop > stopSticky) {
-      stopOffset = $stopElement.offset().top - coverOffset - $startElement.height() - textPadding;
-      $startElement.removeClass(stickyClass);
-      $startElement.css({
-        top: stopOffset + 'px',
-        position: 'absolute',
-        left: '-100px'
-      });
-    } else {
-      $startElement.removeClass(stickyClass).attr('style', null);
-    }
-  });
-
+      } else {
+        $startElement.removeClass(stickyClass).attr('style', null);
+      }
+    });
+  }
 });
